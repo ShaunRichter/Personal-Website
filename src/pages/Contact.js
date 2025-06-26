1de5f8b61d4d94e3991d7bd31f83b96257e6cf5d
Shaun Richter <Shaun.Richter@collins.com>
2025-06-26 14:07:35 -0500

    Updated contact page to not have eslint warning

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,8 +7,8 @@ const Contact = () => {
       <p>Personal Email: shaunrichter547@gmail.com</p>
       <p>School Email: shaun-richter@uiowa.edu</p>
       <p>Phone Number: 951-306-8200</p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/shaun-richter-a90b412b6/" target="_blank">My Profile</a></p>
-      <p>GitHub: <a href="https://github.com/ShaunRichter" target="_blank">My Profile</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/shaun-richter-a90b412b6/" target="_blank" rel="noreferrer">My Profile</a></p>
+      <p>GitHub: <a href="https://github.com/ShaunRichter" target="_blank" rel="noreferrer">My Profile</a></p>
     </div>
   );
 };
