703aa00deaf24da11518ab3d00e70f6e321a4af9
Shaun Richter <Shaun.Richter@collins.com>
2025-06-26 15:14:14 -0500

    Use icons for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,46 @@
 import React from "react";
+import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from "react-icons/fa";
+import "../styles/Contact.css";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact-container">
       <h1>Contact Me</h1>
-      <p>Personal Email: shaunrichter547@gmail.com</p>
-      <p>School Email: shaun-richter@uiowa.edu</p>
-      <p>Phone Number: 951-306-8200</p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/shaun-richter-a90b412b6/" target="_blank" rel="noreferrer">My Profile</a></p>
-      <p>GitHub: <a href="https://github.com/ShaunRichter" target="_blank" rel="noreferrer">My Profile</a></p>
+      <div className="icon-row">
+        <a
+          href="https://www.linkedin.com/in/shaun-richter-a90b412b6/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin className="contact-icon linkedin" />
+        </a>
+
+        <a
+          href="https://github.com/ShaunRichter"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub className="contact-icon github" />
+        </a>
+      </div>
+      <div className="contact-details">
+        <p>
+          <FaEnvelope className="detail-icon" />{" "}
+          <a href="mailto:shaunrichter547@gmail.com">
+            shaunrichter547@gmail.com
+          </a>
+        </p>
+        <p>
+          <FaEnvelope className="detail-icon" />{" "}
+          <a href="mailto:shaun-richter@uiowa.edu">
+            shaun-richter@uiowa.edu
+          </a>
+        </p>
+        <p>
+          <FaPhone className="detail-icon" />{" "}
+          <a href="tel:9513068200">(951) 306-8200</a>
+        </p>
+      </div>
     </div>
   );
 };
