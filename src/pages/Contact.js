9459cbf52e4f2297654fe9144cf01ff161233d80
Shaun Richter <shaunrichter547@gmail.com>
2025-06-26 13:41:36 -0500

    Added initial contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,8 +4,11 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact Me</h1>
-      <p>Email: your.email@example.com</p>
-      <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile" target="_blank">My Profile</a></p>
+      <p>Personal Email: shaunrichter547@gmail.com</p>
+      <p>School Email: shaun-richter@uiowa.edu</p>
+      <p>Phone Number: 951-306-8200</p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/shaun-richter-a90b412b6/" target="_blank">My Profile</a></p>
+      <p>GitHub: <a href="https://github.com/ShaunRichter" target="_blank">My Profile</a></p>
     </div>
   );
 };
