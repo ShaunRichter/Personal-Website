0eed45bce17baf368a0b36010b53b8dd39cee846
Shaun Richter <Shaun.Richter@collins.com>
2025-06-27 15:47:22 -0500

    Updated home page, still styling improvements to be made

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
 import React from "react";
+import homeLogo from "../assets/software-engineer.svg";
+import Type from "../components/Type";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to My Portfolio</h1>
-      <p>Hi, I'm Shaun Richter, a Software Developer.</p>
-    </div>
+    <section className="home-section" id="home">
+      <div className="home-content">
+        {/* ---------- LEFT SIDE: Text ------------------------------------- */}
+        <div className="home-left">
+          <h1 className="greeting">
+            Hey there{" "}
+            <span className="wave" role="img" aria-label="wave">
+              👋
+            </span>
+          </h1>
+
+          <h1 className="name-heading">
+            I'M<strong className="main-name"> SHAUN RICHTER</strong>
+          </h1>
+
+          <div className="typewriter">
+            <Type />
+          </div>
+        </div>
+
+        {/* ---------- RIGHT SIDE: Image ----------------------------------- */}
+        <div className="home-image">
+          <img src={homeLogo} alt="Software Engineer" />
+        </div>
+      </div>
+    </section>
   );
 };
 
