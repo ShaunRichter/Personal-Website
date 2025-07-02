06798595aff6c1c9da473115698f3f3a3471aad1
Shaun Richter <Shaun.Richter@collins.com>
2025-07-02 16:07:03 -0500

    Added hyperlink to resume from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = () => {
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Resume</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
