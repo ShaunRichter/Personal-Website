dfd8bb5279241d607bcb24cc059f77f4496fd3b6
Shaun Richter <Shaun.Richter@collins.com>
2025-07-07 14:04:26 -0500

    Updated navbar to be centered, use bootstrap, and have a fixed position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Navbar.css";
 
-const Navbar = () => {
+const CustomNavbar = () => {
+  const [navScrolled, setNavScrolled] = useState(false);
+
+  const handleScroll = () => {
+    setNavScrolled(window.scrollY >= 20);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <ul className="navbar-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Resume</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar
+      expand="md"
+      className={`custom-navbar ${navScrolled ? "scrolled" : ""}`}
+    >
+      <Container className="navbar-container">
+        <Nav className="navbar-links mx-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#contact">Resume</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default CustomNavbar;
